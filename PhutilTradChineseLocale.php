16df91a1a39052b2dd17599aa201d2e32fb18b0b
Chien Wei Lin <cwlin0416@gmail.com>
2016-05-16 11:19:31 +0800

    add Traditional Chinese locale

---
+++ PhutilTradChineseLocale.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The default Chinese locale.
+ */
+final class PhutilTradChineseLocale extends PhutilLocale {
+  public function getLocaleCode() {
+    return 'zh_Hant';
+  }
+  public function getLocaleName() {
+    return pht('Chinese (Traditional)');
+  }
+}
+
