76a8fca1004374666b89d983cea79c2d5b4da088
Chien Wei Lin <cwlin0416@gmail.com>
2017-03-16 16:29:26 +0800

    Fix extract.sh

--- extract.sh
+++ extract.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
+I18N_STRING_FILE=/usr/local/lib/php/phabricator/src/.cache/i18n_strings.json
 TEMP_PATH=$PWD
 cd /usr/local/lib/php/ && phabricator/bin/i18n extract
-echo $TEMP_PATH
 cd $TEMP_PATH
-php -f parseI18nStrings.php /usr/local/lib/php/phabricator/src/.cache/i18n_strings.json > PhabricatorTradChineseTranslation.php.new
+php -f parseI18nStrings.php $I18N_STRING_FILE > PhabricatorTradChineseTranslation.php.new
 mv PhabricatorTradChineseTranslation.php PhabricatorTradChineseTranslation.php.old
 mv PhabricatorTradChineseTranslation.php.new PhabricatorTradChineseTranslation.php
 
